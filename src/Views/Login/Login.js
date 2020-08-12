d4acfa66a358b4ec94edf6648abfdde34989164c
“Higo <hglbsm@gmail.com>
2020-08-12 00:06:12 -0300

    D - Auth - Add async/await structure

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -2,13 +2,19 @@ import React, { Fragment, useState } from 'react';
 import { Text, TextInput, Button, View } from 'react-native';
 
 import styles from './styles';
+import login from '../../../src/api/login';
 
 const Login = () => {
   const [ user, setUser ] = useState('');
   const [ pass, setPass ] = useState('');
+  const [ errorMessage, setErrorMessage ] = useState('');
 
-  const login = () => {
-    console.warn('Login');
+  const login = async () => {
+    try {
+      await login(user, pass);
+    } catch(err) {
+      setErrorMessage(err.message);
+    }
   }
   
   return (
@@ -25,6 +31,7 @@ const Login = () => {
           onChangeText={text => setPass(text)}
           secureTextEntry
         />
+        <Text>{errorMessage}</Text>
       </View>
       <View style={styles.primaryButton}>
         <Button title="Entrar" onPress={login} />
---
+++ src/api/login.js
@@ -0,0 +1,24 @@
+const login = async (user, pass) => {
+  let url = 'localhost';
+
+  const headerHTTP = {
+    method: 'POST',
+    body: JSON.stringify({
+      username: user,
+      password: pass
+    }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  const response = await fetch(`http://${url}:3030/users/login`,
+    headerHTTP);
+  
+  if (response.ok) 
+    console.warn('Login aceito');
+  else 
+    throw new Error('Não foi possível efetuar o login.');
+};
+
+export default login;
