c61a960a5bb388ce8fe4806cbb7cdade3585899b
“Higo <hglbsm@gmail.com>
2020-08-13 10:48:09 -0300

    D - Component - Add order item state for active or inactive definition

--- src/Components/OrderItem/OrderItem.js
+++ src/Components/OrderItem/OrderItem.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 import styles from './styles';
 
-const OrderItem = ({ data }) => (
-  <View style={styles.orderItem}>
-    <Text style={styles.text}>No #{data.number}</Text>
-    <Text style={styles.label}>Realizado em {data.date}</Text>
-    <Text style={styles.label}>Cliente</Text>
-    <Text style={styles.text}>{data.clientName}</Text>
-    <Text style={styles.label}>Endereço de entrega</Text>
-    <Text style={styles.text}>{data.address}</Text>
-  </View>
-);
+const OrderItem = ({ data }) => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  
+  return (
+    <View style={[styles.container, toggleCheckBox ? styles.active : styles.inactive]}>
+      <Text style={styles.text}>No #{data.number}</Text>
+      <Text style={styles.label}>Realizado em {data.date}</Text>
+      <Text style={styles.label}>Cliente</Text>
+      <Text style={styles.text}>{data.clientName}</Text>
+      <Text style={styles.label}>Endereço de entrega</Text>
+      <Text style={styles.text}>{data.address}</Text>
+      <View style={styles.horizontalRule} />
+      <View style={styles.bar}>
+        <View style={styles.status}>
+          <Text style={styles.barText}>Aguardando entrega</Text>
+        </View>
+        <TouchableOpacity style={styles.more}>
+          <Text style={styles.barText}>Ver mais</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.checkBox}>
+        <CheckBox
+          boxType={'square'}
+          onTintColor={'#00A349'}
+          onCheckColor={'#FFF'}
+          onFillColor={'#00A349'}
+          disable={false}
+          value={toggleCheckBox}
+          onValueChange={(newValue) => setToggleCheckBox(newValue)}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default OrderItem;
--- src/Components/OrderItem/styles.js
+++ src/Components/OrderItem/styles.js
@@ -2,8 +2,9 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 const screenWidth = Dimensions.get('screen').width;
 const styles = StyleSheet.create({
-  orderItem: {
+  container: {
     backgroundColor: "#FFF",
+    borderWidth: 3,
     borderRadius: 8,
     shadowColor: '#00000033',
     shadowOffset: { width: 0, height: 2 },
@@ -11,21 +12,58 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     marginBottom: 15,
     padding: 15,
-    width: screenWidth * 0.9
+    width: screenWidth * 0.9,
+  },
+
+  active: {
+    borderColor: "#00A349"
+  },
+
+  inactive: {
+    borderColor: "transparent"
   },
 
   text: {
     fontSize: 14,
     fontWeight: "bold",
     color: "#2B2D2E",
-    marginBottom: 10
+    marginBottom: 10,
   },
 
   label: {
     fontSize: 12,
     color: "#787B7D",
-    marginBottom: 10
-  }
+    marginBottom: 10,
+  },
+
+  horizontalRule: {
+    borderBottomColor: "#DDD",
+    borderBottomWidth: 1,
+  },
+
+  bar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 15,
+  },
+
+  status: {
+    backgroundColor: "#B8E8FF",
+    padding: 7,
+    borderRadius: 30,
+  },
+  
+  barText: {
+    color: "#0070A3",
+    fontSize: 12,
+  },
+
+  checkBox: {
+    position: "absolute",
+    top: 15,
+    right: 15,
+  },
 });
 
 export default styles;
