7b3f275f08a2b3faf3973741989801f444cf74d5
“Higo <hglbsm@gmail.com>
2020-08-18 13:36:09 -0300

    M - View - Add the order detail style

--- src/Views/OrderDetails/OrderDetails.js
+++ src/Views/OrderDetails/OrderDetails.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
 
 import styles from './styles';
 
 const OrderDetails = ({ route: { params } }) => {
+  const [ listItems, setListItems ] = useState('');
+
+  useEffect(() => {
+    const fetchData = () => {
+      const result = [
+        { key: "01", image: "../../res/img/account.png", text: "Biscoito Cream Cracker Fortaleza 400g x 30"},
+        { key: "02", image: "../../res/img/account.png", text: "Massa para Tapioca Amafil 1kg x 24"},
+        { key: "03", image: "../../res/img/account.png", text: "Refrigerante Guaraná Antarctica 2 Litros x 6"},
+      ];
+  
+      setListItems(result);
+    };
+
+    fetchData();
+  }, []);
+  
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Image style={styles.itemImage} source={require("../../res/img/account.png")} />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+  
   return(
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.info}>
-        <Text>Nº { params.item.number }</Text>
-        <Text>Realizado em { params.item.date}</Text>
-        <Text>Origem</Text>
-        <Text>Loja </Text>
-        <Text>Cliente</Text>
-        <Text>{ params.item.clientName }</Text>
-        <Text>Endereço de entrega</Text>
-        <Text>{ params.item.address }</Text>
+        <Text style={styles.text}>Nº #{ params.item.number }</Text>
+        <Text style={styles.label}>Realizado em { params.item.date}</Text>
+        <Text style={styles.label}>Origem</Text>
+        <Text style={styles.text}>Loja </Text>
+        <Text style={styles.label}>Cliente</Text>
+        <Text style={styles.text}>{ params.item.clientName }</Text>
+        <Text style={styles.label}>Endereço de entrega</Text>
+        <Text style={styles.text}>{ params.item.address }</Text>
+        <Text style={styles.status}>Aguardando entrega</Text>
       </View>
+      
+      <View style={styles.divider} />
+
       <View style={styles.list}>
-        
+        <Text style={styles.label}>Itens</Text>
+        <FlatList 
+          data={listItems}
+          keyExtractor={item => item.key}
+          renderItem={this.renderItem}
+        />
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
--- src/Views/OrderDetails/styles.js
+++ src/Views/OrderDetails/styles.js
@@ -3,15 +3,65 @@ import { StyleSheet, Dimensions } from 'react-native';
 const screenWidth = Dimensions.get('screen').width;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#FFF",
+    flex: 1,
   },
 
   info: {
     width: screenWidth * 0.9,
     alignSelf: "center",
-    paddingVertical: 15
-  }, 
+    marginVertical: 15,
+  },
+
+  text: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#2B2D2E",
+    marginBottom: 10,
+  },
+
+  label: {
+    fontSize: 12,
+    color: "#787B7D",
+    marginBottom: 10,
+  },
+
+  status: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    fontSize: 12,
+    color: "#787B7D",
+  },
+
+  divider: {
+    borderBottomColor: "#CCC",
+    borderBottomWidth: 1,
+  },
+
+  list: {
+    width: screenWidth * 0.9,
+    alignSelf: "center",
+    marginVertical: 15,
+  },
+
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+
+  itemImage: {
+    width: 35,
+    height: 35,
+    marginRight: 10,
+  },
+
+  itemText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#2B2D2E",
+  },  
 });
 
 export default styles;
