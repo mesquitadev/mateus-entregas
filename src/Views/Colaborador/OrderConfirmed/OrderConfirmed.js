472817cca22e57b613cbd7d209fd43470b9fbd3b
Silvio Ricardo <contas@silvioricardo.com.br>
2020-08-31 17:38:58 -0300

    C - OrderConfirmed - Resolução de Conflitos

--- src/Views/Colaborador/OrderConfirmed/OrderConfirmed.js
+++ src/Views/Colaborador/OrderConfirmed/OrderConfirmed.js
@@ -1,37 +1,8 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-=======
 import React from 'react';
->>>>>>> 8b6c58277b8e2b7bfa2023436b4138717c311a5c
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-<<<<<<< HEAD
-const OrderConfirmed = ({ route: { params }, navigation: { navigate } }) => {
-
-    params=""
-  return(
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.touchable} disabled>
-        <Image style={styles.touchableImg} source={require('../../../res/img/ligar.png')} />
-        <Text style={styles.touchableText}>
-          Entrega Confirmada
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.buttonBar}>
-      <TouchableOpacity
-        style={ styles.selectedItemsButton }
-        onPress={() => navigate('OrderList')}>
-        <Text style={styles.selectedItemsButtonText}>
-          {`Ver pedidos selecionados`}
-        </Text>
-      </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
-=======
 const OrderConfirmed = ({navigation}) => {
     return (
         <View style={styles.container}>
@@ -57,6 +28,5 @@ const OrderConfirmed = ({navigation}) => {
 
     );
 }
->>>>>>> 8b6c58277b8e2b7bfa2023436b4138717c311a5c
 
 export default OrderConfirmed;
