67fc7a70a1b0dc7ef5ce8ffca9bb6ff02e83052e
deeborges <deyvisonsilvaborges@gmail.com>
2020-09-03 09:51:51 -0300

    M - Service - cadastro dados pessoais

--- src/services/deliverymanRegister.js
+++ src/services/deliverymanRegister.js
@@ -1,32 +1,21 @@
 import api from './api';
 
-const deliverymanRegister = async (
-  name,
-  formatedCPF,
-  cnh,
-  formatedDataNascimento,
-  formatedTelefone,
-  email,
-) => {
-  const bodyHTTP = {
-    nome: name,
-    cpf: formatedCPF,
-    cnh: cnh,
-    dataNascimento: formatedDataNascimento,
-    email: email,
-    telefone: formatedTelefone,
+const deliverymanRegister = async (payload) => {
+  const data = {
+    nome: payload.nome,
+    cpf: payload.cpf,
+    cnh: payload.cnh,
+    dataNascimento: payload.dataNascimento,
+    email: payload.email,
+    telefone: payload.telefone,
     log: {
       ip: '',
       dispositivo: '',
       localizacao: '',
     },
   };
-  try {
-    const response = await api.post(`/entregador`, bodyHTTP);
-    return response;
-  } catch (error) {
-    throw new Error('Erro ao chamar o serviço de cadastro.')
-  }
+  const response = await api.post('/entregador', JSON.stringify(data));
+  return response;
 };
 
 export default deliverymanRegister;
