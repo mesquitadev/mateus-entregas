e591b30b536c6aea2d7a48e16e415c6ef9c4041d
deeborges <deyvisonsilvaborges@gmail.com>
2020-09-04 14:42:57 -0300

    C - AnotherPearson - merge

--- src/Views/Entregador/ReceiptByAnotherPerson/ReceiptByAnotherPerson.js
+++ src/Views/Entregador/ReceiptByAnotherPerson/ReceiptByAnotherPerson.js
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
-import {Alert, Text, TextInput, TouchableOpacity, View} from 'react-native';
-
-import AsyncStorage from '@react-native-community/async-storage';
+import React, {useState} from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {TextInputMask} from 'react-native-masked-text';
 
 import styles from './styles';
+import Validators from '../../../Utils/validators';
 
-const ReceiptByAnotherPerson = ({ navigation }) => {
-  const [ name, setName ] = useState('');
-  const [ cpf, setCpf ] = useState('');
-  
+const ReceiptByAnotherPerson = ({navigation}) => {
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+
+  const formatedCPF = user.replace(/[^\d]/g, '');
+
   const saveReceipt = async () => {
     try {
-      await AsyncStorage.setItem('receipt_by_another_person', 
-        JSON.stringify({ name: name, cpf: cpf, situacao: 5 })); 
-        
+      Validators.validateCpf(formatedCPF);
+      await AsyncStorage.setItem(
+        'receipt_by_another_person',
+        JSON.stringify({name: name, cpf: cpf, situacao: 5}),
+      );
+
       navigation.navigate('StartDelivery');
     } catch (error) {
       console.warn(error.message);
     }
   };
-  
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.inputs}
         placeholder="Nome completo"
-        onChangeText={text => setName(text)}
+        onChangeText={(text) => setName(text)}
       />
       <TextInputMask
-        type={"cpf"}
+        type={'cpf'}
         placeholder="CPF"
         value={cpf}
-        onChangeText={text => setCpf(text.replace(/[^\d]+/g,''))}
+        onChangeText={(text) => setCpf(text.replace(/[^\d]+/g, ''))}
         style={styles.inputs}
         maxLength={14}
       />
-      <TouchableOpacity style={styles.btnPrimary} onPress={saveReceipt}>
-        <Text style={styles.btnPrimaryText}>
-          Confirmar
-        </Text>
+      <TouchableOpacity style={styles.btnPrimary} onPress={postReceipt}>
+        <Text style={styles.btnPrimaryText}>Confirmar</Text>
       </TouchableOpacity>
     </View>
   );
